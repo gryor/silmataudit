1a490826a88d0631579d7e4b99f1f11a83469519
Wilhelm Matilainen <wilhelmmatilainen@users.noreply.github.com>
2017-10-17 12:56:44 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# SilmÃ¤taudit documentation build configuration file, created by
+# Silmätaudit documentation build configuration file, created by
 # sphinx-quickstart on Tue Oct 17 11:13:17 2017.
 #
 # This file is execfile()d with the current directory set to its
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'SilmÃ¤taudit'
+project = 'Silmätaudit'
 copyright = '2017, Wilhelm Matilainen'
 author = 'Wilhelm Matilainen'
 
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -115,7 +115,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Silmtauditdoc'
+htmlhelp_basename = 'Silmatauditdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -142,7 +142,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Silmtaudit.tex', 'SilmÃ¤taudit Documentation',
+    (master_doc, 'Silmataudit.tex', 'Silmätaudit Documentation',
      'Wilhelm Matilainen', 'manual'),
 ]
 
@@ -152,7 +152,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'silmtaudit', 'SilmÃ¤taudit Documentation',
+    (master_doc, 'silmataudit', 'Silmätaudit Documentation',
      [author], 1)
 ]
 
@@ -163,8 +163,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Silmtaudit', 'SilmÃ¤taudit Documentation',
-     author, 'Silmtaudit', 'One line description of project.',
+    (master_doc, 'Silmataudit', 'Silmätaudit Documentation',
+     author, 'Silmataudit', 'One line description of project.',
      'Miscellaneous'),
 ]
 
